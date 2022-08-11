38b03cc354cd1386ef8bc214c3449c2962331c6a
Nathan Brownstein <nana8901@gmail.com>
2022-08-11 13:37:17 -0400

    adding basic CRUD skeleton to user_controller, added email column to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  
+  def create
+    code
+  end
+
+  def index
+    code
+  end
+
+  def show
+    code
+  end
+
+  def patchCheckLater
+    code
+  end
+
+  def destroy
+    code
+  end
 end
